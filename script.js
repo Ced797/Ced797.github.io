bb68a16e678efcfae27c7bb02a8ae951cf99587d
Ced797 <littlec5@winthrop.edu>
2020-10-01 16:19:11 -0400

    hi

---
+++ script.js
@@ -0,0 +1,71 @@
+$(function(){
+	
+	$(".dropdwn").hide();
+	
+	$(document).ready(function(){
+		$("#review").click(function(){
+			$(".dropdwn").fadeToggle(30);
+		});
+	});
+});
+///////////////////////////////////////////
+$(function(){
+	$("#faqlst2").hide();
+	
+	$(document).ready(function(){
+		$("#one").click(function(){
+			$("#faqlst2").fadeToggle(20);
+		});
+	});
+});
+$(function(){
+	$("#faqlst3").hide();
+	
+	$(document).ready(function(){
+		$("#two").click(function(){
+			$("#faqlst3").fadeToggle(20);
+		});
+	});
+});
+$(function(){
+	$("#faqlst4").hide();
+	
+	$(document).ready(function(){
+		$("#three").click(function(){
+			$("#faqlst4").fadeToggle(20);
+		});
+	});
+});
+$(function(){
+	$("#faqlst5").hide();
+	
+	$(document).ready(function(){
+		$("#four").click(function(){
+			$("#faqlst5").fadeToggle(20);
+		});
+	});
+});
+
+$(function(){
+	$("#faqlst0").hide();
+	
+	$(document).ready(function(){
+		$("#zero").click(function(){
+			$("#faqlst0").fadeToggle(20);
+		});
+	});
+});
+///////////////////////////////////////////////////
+/*$(function(){
+	$("#walkmansTypes").hide();
+	
+	$(document).ready(function(){
+		$().click(function(){
+			$("").fadeIn(30);
+		});
+	});
+});
+for(var i=0; i< ; i++)
+{
+
+}*/
